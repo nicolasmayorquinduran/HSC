c36975b923bc539fd0082afeddb7d774c2f6b5f5
nicolas mayorquin <nicolasmayorquinduran@gmail.com>
2023-07-01 19:09:04 -0500

    add typos

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,76 @@ import { getHierarchyChanelsFromRGBcompose } from "../core/functions";
 import {ChangeEvent} from 'react'
 
 
-function Home() {
+type canalDominante = {
+    [indice in indicesCanalDominanteEnum]:canalInterface | {indice:0}
+}
 
+
+type canales = {
+    [tipoCanal in canalesEnum]:canalInterface
+}
+
+type jerarquias = {
+    [tipoJerarquia in jerarquiasEnum]:canalInterface | canalDominante | null
+}
+
+enum indicesCanalDominanteEnum {
+    x = 'x',
+    y = 'y'
+}
+
+enum canalesEnum {
+    R = 'R',
+    G = 'G',
+    B = 'B'
+}
+
+enum jerarquiasEnum {
+    d ='d',
+    m = 'm',
+    n = 'n',
+    i = 'i'
+}
+
+interface canalInterface {
+    id:canalesEnum,
+    claridadTonal:number,
+    alcanceTonal:number,
+    indice: 1 | 3 | 5
+    niveles:number
+}
+
+type canalParamsTuple = [canalInterface[keyof canalInterface]];
+
+// const crearCanal = (canal: canalParamsTuple):canalInterface =>{
+//     return canal.reduce((acc, el) => ({...acc, [el]:el}), {} as canalInterface)
+// }
+
+// const ejemploCanales: canales = {
+//     [canalesEnum.R]: crearCanal([canalesEnum.R,1,1,1,1]),
+//     [canalesEnum.G]: crearCanal([1,1,1,1,1]),
+//     [canalesEnum.B]: crearCanal([1,1,1,1,1])
+// }
+// console.log(ejemploCanales);
+
+const canalDominanteEjemplo: canalDominante = {
+    x:{
+        id:canalesEnum.B,
+        claridadTonal:0,
+        alcanceTonal:1,
+        indice:1,
+        niveles:255
+    },
+    y:{
+        indice:0,
+    }
+}
+
+
+
+function Home() {
+    console.log(canalDominanteEjemplo);
+    
     const u = 255
 
     const [rgbCompose, setRgbCompose] = useState({
